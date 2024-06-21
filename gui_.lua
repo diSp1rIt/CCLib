a41652f14fbf391710d49464b3f02d42a16ca0d4
levarrr <lewartamonov@gmail.com>
2024-06-21 13:44:05 +0200

    upper border

--- gui_.lua
+++ gui_.lua
@@ -1,6 +1,13 @@
+-- preparing screen
 term.clear()
-
 width, height = term.getSize()
+term.setCursorPos(1, 1)
 
-print(width, height)
+term.write("/")
+for i = 2, width do
+    term.setCursorPos(i, 1)
+    term.write("-")
+end
+term.setCursorPos(width, 1)
+term.write("\\")
 
