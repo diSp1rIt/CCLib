9c178727a024394c561afa194363cb7a5a417a96
levarrr <lewartamonov@gmail.com>
2024-06-21 13:54:07 +0200

    add borders

--- gui_.lua
+++ gui_.lua
@@ -11,3 +11,17 @@ end
 term.setCursorPos(width, 1)
 term.write("\\")
 
+for i = 2, height do
+    term.setCursorPos(1, i)
+    term.write("|")
+    term.setCursorPos(width, i)
+    term.write("|")
+end
+
+term.write("\\")
+for i = 2, width do
+    term.setCursorPos(i, height)
+    term.write("-")
+end
+term.setCursorPos(width, height)
+term.write("/")
