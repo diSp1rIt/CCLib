d63adb028c21189a4618b02a90683c7274589172
levarrr <lewartamonov@gmail.com>
2024-06-22 23:23:33 +0200

    update menu

--- menu.lua
+++ menu.lua
@@ -2,38 +2,33 @@ local repository = "https://raw.githubusercontent.com/diSp1rIt/CCLib/main/"
 
 local function check()
     term.setCursorPos(1, 1)
-    local nescessary_libs = {"update.lua"}
+    local nescessary_libs = {"update.lua", "install.lua", "remove.lua"}
 
     shell.run("mkdir commands")
 
     for _, lib  in pairs(nescessary_libs) do
         local file, err = io.open("commands/" .. lib, "r")
         if not file then
-            print("[-] Missing update command")
+            print("[-] Missing " .. lib)
             print("[ ] Starting download")
             local file, err = io.open("commands/".. lib, "w")
             if err then
                 print("Couldn't open file")
             else
-                local res = http.get(repository .. "commands/update.lua") 
+                local res = http.get(repository .. "commands/" .. lib) 
                 file:write(res.readAll())
                 io.close(file)
-                print("[+] Update command downloaded successfully")
+                print("[+] Command downloaded successfully")
             end
         else
             io.close(file)
-            print("[+] Update exists")
+            print("[+] " .. lib .. " exists")
         end
     end
+    print("Continue in 5 sec...")
+    sleep(5)
 end
 
 term.clear()
 check()
 
--- local options = {"Update CCGUI" = "update.lua"}
-
--- local funtion render_main()
---     for i = 0, __len(options) do
-        
---     end
--- end
